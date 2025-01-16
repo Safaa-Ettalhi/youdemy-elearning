52e0c57870cae22f2ad8161bb8832ddcfa73d417
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-16 18:31:26 +0100

    add page succes qui saffiche apres que luser est inscrit dans un cours

---
+++ etudiant/succes.php
@@ -0,0 +1,157 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogue des cours - Youdemy</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'youdemy': '#ff7900',
+                        'youdemy-hover': '#ff9533',
+                    }
+                }
+            }
+        }
+    </script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="min-h-screen flex flex-col">
+
+    <nav class="fixed w-full bg-white/95 backdrop-blur-sm z-50">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-20">
+                <div class="flex items-center">
+                    <a href="#" class="flex-shrink-0">
+                        <img src="./SAFAA BB.svg" alt="Safaa Ettalhi" >
+                    </a>
+                </div>
+                <div class="flex sm:hidden   items-center">
+                    <!-- Burger Icon for Mobile -->
+                    <button id="burger-icon" class="text-gray-600 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="hidden sm:flex sm:items-center  sm:justify-center sm:space-x-8  text-xl" id="menu">
+                    <!-- <a href="#Accueil" class="text-gray-600 hover:text-gray-900">Accueil</a> -->
+                    <a href="./catalogecours.php" class="text-gray-600 hover:text-gray-900">Cours</a>
+                    <a href="./mescours.php" class="text-gray-600 hover:text-gray-900">Mes cours</a>
+                    
+                    
+                    
+                </div>
+                <div class="hidden sm:flex sm:items-center sm:justify-center sm:gap-2">
+                <button class="bg-white hover:bg-orange-500 hover:text-white text-l text-orange-400 border border-orange-400 px-6 py-2 rounded-full transition duration-300">
+                    <a href="../profil.php">Profil</a>
+                </button>
+                <button class="w-full bg-orange-400 hover:bg-orange-500 text-white px-6 py-2 rounded-full transition duration-300 ">
+                   <a href="../deconnecter.php">Deconnecter</a> 
+                </button></div>
+            </div>
+        </div>
+    </nav>
+
+
+
+       
+
+       
+
+        <!-- Hero Section -->
+        <section class="hero flex pt-10 flex-col items-center justify-center mt-20">
+            <div class="mb-4">
+                <img src="./done.png" alt="succes" height="400px" width="400px"
+                    class="mx-auto ">
+                   
+            </div>
+            <p class="text-green-700 text-xl font-bold mb-4">Vous êtes inscrit à ce cour avec succes</p>
+            <div class="mt-6 flex gap-4">
+            <a href="./mescours.php"
+                        class="mx-auto px-6 py-3 bg-orange-400 text-white font-semibold rounded-full hover:bg-orange-500 transition">
+                        Voir Mes Cours
+            </a>
+            <a href="./catalogecours.php"
+                        class="mx-auto px-6 py-3 bg-white text-orange-400 border border-orange-400 font-semibold rounded-full hover:bg-orange-500 hover:text-white transition">
+                        Voir Cours
+            </a>
+                   
+            </div>
+        </section>
+
+    </div>
+
+
+    <!-- Footer Section -->
+
+    <footer class="bg-gray-100 py-20 px-8 pt-16 pb-8">
+        <div class="container max-w-7xl mx-auto px-4">
+            <div class="grid md:grid-cols-3 gap-8 md:gap-0 mb-8">
+                <!-- Brand -->
+                <div>
+                    <a href="#" class="text-2xl font-bold text-orange-400 mb-4 inline-block">Youdemy</a>
+                    <p class="text-gray-600">
+                        Youdemy est votre passerelle vers l'éducation en ligne, offrant une large gamme de cours pour atteindre vos objectifs d'apprentissage.
+                    </p>
+                </div>
+
+                <!-- Quick Links -->
+                <div class="md:ml-40">
+                    <h3 class="font-bold text-lg mb-4">Quick Links</h3>
+                    <ul class="space-y-2">
+                        <li><a href="#" class="text-gray-600 hover:text-gray-900">Home</a></li>
+                        <li><a href="#" class="text-gray-600 hover:text-gray-900">Courses</a></li>
+                        <li><a href="#" class="text-gray-600 hover:text-gray-900">Contact</a></li>
+                        <li><a href="#" class="text-gray-600 hover:text-gray-900">Blog</a></li>
+                    </ul>
+                </div>
+
+
+                <!-- Contact Us -->
+                <div class="md:ml-40">
+                    <h3 class="font-bold mb-4">Contact Us</h3>
+                    <ul class="space-y-2 text-gray-600">
+                        <li>Phone : 06-6666-6666</li>
+                        <li>Email : safaeettalhi@gmail.com</li>
+                        <li>Address : Safi , Maroc</li>
+                    </ul>
+                </div>
+            </div>
+
+            
+        </div>
+        <!-- Footer Bottom -->
+        <div class="pt-8 border-t border-gray-200">
+            <div class="flex flex-col md:flex-row justify-between items-center">
+                <p class="text-gray-600 mb-4 md:mb-0 text-xl">&copy; 2025 Youdemy. Tous droits réservés.</</p>
+                <div class="flex space-x-4 text-2xl">
+                    <a href="#" class="text-gray-600 hover:text-gray-900">
+                        <i class="ri-facebook-fill"></i>
+                    </a>
+                    <a href="#" class="text-gray-600 hover:text-gray-900">
+                        <i class="ri-twitter-fill"></i>
+                    </a>
+                    <a href="#" class="text-gray-600 hover:text-gray-900">
+                        <i class="ri-instagram-fill"></i>
+                    </a>
+                    <a href="#" class="text-gray-600 hover:text-gray-900">
+                        <i class="ri-linkedin-fill"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+
+
+</html>
