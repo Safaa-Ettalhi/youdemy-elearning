2044ddc599517ed1ea5a56521cfb8340f06fe350
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-16 01:31:13 +0100

    add logo in page login

--- login.php
+++ login.php
@@ -69,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="flex justify-between h-20">
                 <div class="flex items-center">
                     <a href="#" class="flex-shrink-0">
-                        <span class="text-3xl font-bold text-orange-400">Youdemy</span>
+                    <img src="./SAFAA BB.svg" alt="Safaa Ettalhi" >
                     </a>
                 </div>
                 <div class="flex sm:hidden items-center">
@@ -238,4 +238,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </footer>
     <script src="scriptF.js"></script>
 </body>
-<.php>
