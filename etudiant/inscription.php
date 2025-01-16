7fe07c66d96b7df851a23af9fcf89e3644553ac7
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-16 17:27:38 +0100

    add inscreption qui permet dinscrire a un cours selectionner

---
+++ etudiant/inscription.php
@@ -0,0 +1,46 @@
+<?php
+include('../class/db.php');
+
+session_start();
+if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
+    header('Location: ../login.php');
+    exit();
+}
+
+$utilisateur_id = intval($_SESSION['id']); 
+
+$cours_id = isset($_POST['course_id']) ? intval($_POST['course_id']) : null;
+//echo (var_dump($utilisateur_id, $cours_id));
+if (!$cours_id) {
+    die("Cours non spécifié.");
+}
+
+$db = new Database();
+$pdo = $db->getPDO();
+
+$stmt = $pdo->prepare("SELECT id FROM Utilisateur WHERE id = ? AND role = 'Etudiant'");
+$stmt->execute([$utilisateur_id]);
+if ($stmt->rowCount() == 0) {
+    die("L'utilisateur avec cet ID n'existe ");
+}
+
+try {
+    // Vérifier si l'utilisateur est déjà inscrit
+    $stmt = $pdo->prepare("SELECT * FROM Enrollment WHERE user_id = ? AND cours_id = ?");
+    $stmt->execute([$utilisateur_id, $cours_id]);
+
+    if ($stmt->rowCount() > 0) {
+        header('Location: ./warning.php');
+        exit();
+    }
+
+    // Ajouter l'inscription
+    $stmt = $pdo->prepare("INSERT INTO Enrollment (user_id, cours_id, date_inscription) VALUES (?, ?, NOW())");
+    $stmt->execute([$utilisateur_id, $cours_id]);
+
+    header('Location: ./succes.php');
+} catch (PDOException $e) {
+    die("Erreur : " . $e->getMessage());
+}
+?>
+
