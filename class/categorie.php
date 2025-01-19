84b2f90d10d0e3a46a62c073499c95d56d743351
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-20 00:03:55 +0100

    update metoge get

--- class/categorie.php
+++ class/categorie.php
@@ -5,31 +5,19 @@ class Category {
     private $nom;
     private $pdo;
 
-    // Constructeur
     public function __construct($pdo, $id = null, $nom = '') {
         $this->pdo = $pdo;
         $this->id = $id;
         $this->nom = $nom;
     }
 
-  
-   
-
-   
-    public function createCategory($nom) {
-        $stmt = $this->pdo->prepare('INSERT INTO Category (nom) VALUES (?)');
-        return $stmt->execute([$nom]);
-    }
-
-  
 public function getCategories() {
 
     $stmt = $this->pdo->prepare('SELECT * FROM Category ');
     $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);}
-   
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
-    
     public function deleteCategory($id) {
         $stmt = $this->pdo->prepare('DELETE FROM Category WHERE id = ?');
         return $stmt->execute([$id]);
