3c77e35483bdbdbf3eb81ed60338f8fb05d2ae93
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-20 00:07:33 +0100

    delete metode non necaisaire

--- class/enrollement.php
+++ class/enrollement.php
@@ -6,7 +6,6 @@ class Enrollment {
     private $cours_id;
     private $date_inscription;
 
-    
     public function __construct($pdo, $etudiant_id = null, $cours_id = null) {
         $this->pdo = $pdo;
         $this->etudiant_id = $etudiant_id;
@@ -14,27 +13,6 @@ class Enrollment {
         $this->date_inscription = date('Y-m-d');
     }
 
-    
-    public function addEnrollment($course_id, $user_id) {
-        $stmt = $this->pdo->prepare("INSERT INTO Enrollment (etudiant_id, cours_id) VALUES (:user_id,:course_id)");
-        $stmt->bindParam(':user_id', $user_id);
-        $stmt->bindParam(':course_id', $course_id);
-        
-        $stmt->execute();
-    }
-    public function getUserEnrollments($user_id) {
-        $stmt = $this->pdo->prepare("
-            SELECT c.* 
-            FROM enrollment e 
-            JOIN cours c ON e.cours_id = c.id 
-            WHERE e.etudiant_id = :user_id
-        ");
-        $stmt->bindParam(':user_id', $user_id);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    
     public function getEnrollmentsByStudent($etudiant_id) {
     $stmt = $this->pdo->prepare('
         SELECT 
@@ -54,32 +32,16 @@ class Enrollment {
 }
 
 
-    
-    public function getEnrollmentsByCourse($cours_id) {
-    $stmt = $this->pdo->prepare('
-        SELECT Utilisateur.*, Enrollment.date_inscription
-        FROM Enrollment
-        JOIN Utilisateur ON Enrollment.user_id = Utilisateur.id
-        WHERE Enrollment.cours_id = :cours_id AND Utilisateur.role = "Etudiant"
-    ');
-
-    $stmt->bindParam(':cours_id', $cours_id, PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
 public function updateEnrollmentStatus($coursId, $userId, $newStatus = 'Complet') {
         try {
-            
+          
             $query = "UPDATE Enrollment SET status = :status WHERE cours_id = :coursId AND user_id = :userId";
             $stmt = $this->pdo->prepare($query);
 
-            
             $stmt->bindParam(':status', $newStatus, PDO::PARAM_STR);
             $stmt->bindParam(':coursId', $coursId, PDO::PARAM_INT);
             $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
 
-            
             return $stmt->execute();
         } catch (PDOException $e) {
             
@@ -87,11 +49,7 @@ public function updateEnrollmentStatus($coursId, $userId, $newStatus = 'Complet'
             return false;
         }
     }
-    
-    public function setEnrollmentDetails($etudiant_id, $cours_id) {
-        $this->etudiant_id = $etudiant_id;
-        $this->cours_id = $cours_id;
-    }
+  
 }
 
 ?>
