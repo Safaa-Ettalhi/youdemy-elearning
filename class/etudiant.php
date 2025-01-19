df3a0d867b5b5dbcaab117556439515f615d22a2
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-20 00:09:10 +0100

    delete metode non necaisaire

--- class/etudiant.php
+++ class/etudiant.php
@@ -8,17 +8,5 @@ class Etudiant extends User {
 
     }
 
-    
-
-    public function enrollInCourse($cours_id) {
-        $stmt = $this->pdo->prepare('INSERT INTO Enrollment (etudiant_id, cours_id, date_inscription) VALUES (?, ?, NOW())');
-        return $stmt->execute([$this->id, $cours_id]);
-    }
-
-    public function getEnrolledCourses() {
-        $stmt = $this->pdo->prepare('SELECT Cours.* FROM Cours INNER JOIN Enrollment ON Cours.id = Enrollment.cours_id WHERE Enrollment.etudiant_id = ?');
-        $stmt->execute([$this->id]);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 }
 ?>
