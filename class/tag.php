a7db476a91f67ecbe509fb813db78c89b2422f06
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-01-20 02:12:53 +0100

    update tag

--- class/tag.php
+++ class/tag.php
@@ -5,50 +5,16 @@ class Tag {
     private $nom;
     private $pdo;
 
-  
     public function __construct($pdo, $id = null, $nom = '') {
         $this->pdo = $pdo;
         $this->id = $id;
         $this->nom = $nom;
     }
-
-  
-    public function getId() {
-        return $this->id;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-   
-    public function getNom() {
-        return $this->nom;
-    }
-
-    public function setNom($nom) {
-        $this->nom = $nom;
-    }
-
-   
-    public function createTag($nom) {
-        $stmt = $this->pdo->prepare('INSERT INTO Tag (nom) VALUES (?)');
-        return $stmt->execute([$nom]);
-    }
-
-   
     public function getTags() {
         $stmt = $this->pdo->query('SELECT * FROM Tag');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    
-    public function deleteTag($id) {
-        $stmt = $this->pdo->prepare('DELETE FROM Tag WHERE id = ?');
-        return $stmt->execute([$id]);
-    }
-
-   
     public function getTagsByCourseId($course_id) {
         $stmt = $this->pdo->prepare("SELECT t.nom AS tag_nom, t.id AS tag_id 
                                      FROM Cours_Tags ct 
